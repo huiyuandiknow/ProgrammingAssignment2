f63b4bcd06f661c6268707fd0060c107f6c3ce11
Huiyu <huiyu.y@gmail.com>
2015-07-14 17:15:56 -0700

    Add functions makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Date: 07/14/2015
+##
+## This R file contains 2 R functions for Assignment 2 
+## for "Introduction to R" course on Coursera
 
-## Write a short comment describing this function
+
+## makeCacheMatrix creates a "matrix" object that can cache 
+## its inverse. This function creates an matrix object with 4 
+## functions: set, get, setinverse, and getinverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
 
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. If it's been calculated
+## earlier, it'll return the cached matrix. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if (!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
+
+
+## codes for testing 
+# w<- matrix(c(2,1,7,4,5,4,3,4,5),nrow=3,ncol=3)
+# solve(w)
+
+# k<- makeCacheMatrix(w)
+# sum(cacheSolve(k) == solve(w)) == nrow(w)*ncol(w)
+
+
+
